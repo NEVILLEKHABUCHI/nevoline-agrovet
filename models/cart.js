6a7f88932ee2a01dce47ea9d8862d0f0ba8f2fe7
Neville Khabuchi <nevillekhabuchi@gmail.com>
2025-02-13 18:01:33 +0300

    The cart model

---
+++ models/cart.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// cart schema for the cart collection in mongoDB
+const cartSchema = new mongoose.Schema({
+    user_id: { type: Number, required: true },
+    productImage: {type: String, required: true},
+    productName: {type:String, required: true},
+    productPrice: {type: Number, required: true}
+});
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+module.exports = Cart;
